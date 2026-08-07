4ce73edf1c744f1abf4ec8de95179ad96c17ff3b
Mauro Flores <mauroezequielflores48@gmail.com>
2026-08-07 20:02:54 +0000

    Limpieza de carpeta backend para el nuevo MVP

--- backend/services/usuarios.service.js
+++
@@ -1,9 +0,0 @@
-import { getAllUserProfiles } from '../models/userModel.js';
-
-/**
- * Servicio para listar todos los usuarios
- * @returns {Promise<Array>} Lista de usuarios
- */
-export async function listarUsuarios() {
-  return await getAllUserProfiles();
-}
